db8f4ab7b740e975c55a5ea0ee9e94edf241b397
HaeKwang Byun <95038621+haebyun@users.noreply.github.com>
2024-05-16 19:27:13 +0900

    init

---
+++ src/main/java/org/example/tripaicall/TriPAiCallApplication.java
@@ -0,0 +1,13 @@
+package org.example.tripaicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriPAiCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriPAiCallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tripaicall/TriPAiCallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tripaicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriPAiCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
