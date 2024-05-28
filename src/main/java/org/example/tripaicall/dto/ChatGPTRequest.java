82143ccba70541a133c7447b51b9a97dcb5fa96a
haebyun <bhk4392@gmail.com>
2024-05-28 16:30:05 +0900

    feat : model 이름, maxTokens, RequestMessage 클래스 리스트 들을 필드값으로 갖는 ChatGPTRequest 클래스 구현

---
+++ src/main/java/org/example/tripaicall/dto/ChatGPTRequest.java
@@ -0,0 +1,20 @@
+package org.example.tripaicall.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGPTRequest {
+    @JsonProperty("model")
+    private String model;
+    @JsonProperty("messages")
+    private List<RequestMessage> messages;
+    @JsonProperty("max_tokens")
+    private int maxTokens;
+}
