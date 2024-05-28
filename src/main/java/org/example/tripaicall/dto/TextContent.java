a0d675fc08db521327ab587863d276673cfa5093
haebyun <bhk4392@gmail.com>
2024-05-28 16:19:00 +0900

    feat : 내용(질문 혹은 요청)에 해당하는 text를 필드로 갖는 TextContent 구현

---
+++ src/main/java/org/example/tripaicall/dto/TextContent.java
@@ -0,0 +1,17 @@
+package org.example.tripaicall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TextContent extends Content {
+    private String text;
+
+    public TextContent(String type, String text) {
+        super(type);
+        this.text = text;
+    }
+}
