2b5282fb939f2e0ad3780c6ec0361c6ebc8af93f
haebyun <bhk4392@gmail.com>
2024-05-28 16:21:52 +0900

    feat : url 값을 필드로 갖는 ImageUrl 구현, ImageUrl 클래스를 필드로 갖는 ImageContent 클래스 구현

---
+++ src/main/java/org/example/tripaicall/dto/ImageContent.java
@@ -0,0 +1,17 @@
+package org.example.tripaicall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ImageContent extends Content {
+    private ImageUrl image_url;
+
+    public ImageContent(String type, ImageUrl image_url) {
+        super(type);
+        this.image_url = image_url;
+    }
+}
