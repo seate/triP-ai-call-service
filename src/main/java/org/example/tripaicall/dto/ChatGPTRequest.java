220d618141a18271b55525320ffc2c88048c55f1
haebyun <bhk4392@gmail.com>
2024-05-28 16:41:06 +0900

    feat : model, maxTokens, 질문 텍스트, 이미지 url 들을 인자로 받아서 필드 값을 세팅하도록 생성자 구현

--- src/main/java/org/example/tripaicall/dto/ChatGPTRequest.java
+++ src/main/java/org/example/tripaicall/dto/ChatGPTRequest.java
@@ -1,13 +1,15 @@
 package org.example.tripaicall.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Collections;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatGPTRequest {
@@ -17,4 +19,15 @@ public class ChatGPTRequest {
     private List<RequestMessage> messages;
     @JsonProperty("max_tokens")
     private int maxTokens;
+
+    public ChatGPTRequest(String model, int maxTokens, String requestText, String imageUrl) {
+        this.model = model;
+        this.maxTokens = maxTokens;
+
+        TextContent textContent = new TextContent("text", requestText);
+        ImageContent imageContent = new ImageContent("image_url", new ImageUrl(imageUrl));
+
+        RequestMessage message = new RequestMessage("user", List.of(textContent, imageContent));
+        this.messages = Collections.singletonList(message);
+    }
 }
