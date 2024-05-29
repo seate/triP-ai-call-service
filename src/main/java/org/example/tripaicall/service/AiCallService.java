cc5428ff28dcabee342d5d81c7823d466a582be2
haebyun <bhk4392@gmail.com>
2024-05-29 09:12:39 +0900

    feat : image(MultipartFile), requestText 가 주어졌을 때, ChatGPTRequest 클래스를 만든 후 restTemplate Body에 넣어서 Post하고 ChatGPTResponse 를 받아서 return 하는 기능 구현

--- src/main/java/org/example/tripaicall/service/AiCallService.java
+++ src/main/java/org/example/tripaicall/service/AiCallService.java
@@ -1,11 +1,14 @@
 package org.example.tripaicall.service;
 
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.example.tripaicall.dto.request.ChatGPTRequest;
 import org.example.tripaicall.dto.response.ChatGPTResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +22,13 @@ public class AiCallService {
     private final RestTemplate template;
 
     public ChatGPTResponse imageUrlAnalysis(String imageUrl, String requestText) {
+        ChatGPTRequest request = new ChatGPTRequest(apiModel, MAX_TOKENS, requestText, imageUrl);
+        return template.postForObject(apiUrl, request, ChatGPTResponse.class);
+    }
+
+    public ChatGPTResponse imageAnalysis(MultipartFile image, String requestText) throws IOException {
+        String base64Image = Base64.encodeBase64String(image.getBytes());
+        String imageUrl = "data:image/jpeg;base64," + base64Image;
         ChatGPTRequest request = new ChatGPTRequest(apiModel, MAX_TOKENS, requestText, imageUrl);
         return template.postForObject(apiUrl, request, ChatGPTResponse.class);
     }
