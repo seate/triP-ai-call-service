471e6fd6835ead2713e643901f31725919e81a29
seate <86824703+seate@users.noreply.github.com>
2024-06-02 04:37:55 +0900

    refactor: Config 디렉토리로 refactor

--- src/main/java/org/example/tripaicall/Config/OpenAiConfig.java
+++ src/main/java/org/example/tripaicall/Config/OpenAiConfig.java
@@ -1,4 +1,4 @@
-package org.example.tripaicall;
+package org.example.tripaicall.Config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
