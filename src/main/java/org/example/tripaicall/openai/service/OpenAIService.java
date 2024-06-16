3015aa345f6d08900443c7ea1470e7e5817916ae
haebyun <bhk4392@gmail.com>
2024-06-16 16:44:53 +0900

    feat : chatbot 관련 서비스 메서드들 추가

--- src/main/java/org/example/tripaicall/openai/service/OpenAIService.java
+++ src/main/java/org/example/tripaicall/openai/service/OpenAIService.java
@@ -3,8 +3,10 @@ package org.example.tripaicall.openai.service;
 import jakarta.annotation.PostConstruct;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.message.Message;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.example.tripaicall.chatbot.repository.TestRepository;
+import org.example.tripaicall.openai.dto.Message;
+import org.example.tripaicall.openai.dto.TextMessage;
 import org.example.tripaicall.openai.dto.request.ChatGPTRequest;
 import org.example.tripaicall.openai.dto.response.ChatGPTResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,6 +30,7 @@ public class OpenAIService {
     private String openAiApiKey;
 
     private RestClient restClient;
+    private final TestRepository testRepository;
 
     @PostConstruct
     private void restClient() {
@@ -47,7 +50,6 @@ public class OpenAIService {
 
     public ChatGPTResponse imageUrlAnalysis(String imageUrl, String requestText) {
         ChatGPTRequest request = ChatGPTRequest.createImageRequest(apiModel, maxTokens, "user", requestText, imageUrl);
-
         return sendRequest(request);
     }
 
@@ -55,13 +57,32 @@ public class OpenAIService {
         String base64Image = Base64.encodeBase64String(image.getBytes());
         String imageUrl = "data:image/jpeg;base64," + base64Image;
         ChatGPTRequest request = ChatGPTRequest.createImageRequest(apiModel, maxTokens, "user", requestText, imageUrl);
-
         return sendRequest(request);
     }
 
     public ChatGPTResponse textAnalysis(String requestText) {
         ChatGPTRequest request = ChatGPTRequest.createTextRequest(apiModel, maxTokens, "user", requestText);
-
         return sendRequest(request);
+    }
+
+    public List<Message> getMessages(String userId) {
+        return testRepository.getMessages(userId);
+    }
+
+    public ChatGPTResponse chatWithAi(String userId, String chatText) {
+        List<Message> messages = testRepository.getMessages(userId);
+        Message chatMessage = new TextMessage("user", chatText);
+        messages.add(chatMessage);
+        ChatGPTRequest request = ChatGPTRequest.createChatBotRequest(apiModel, maxTokens, messages);
+        ChatGPTResponse response = sendRequest(request);
+        addMessage(messages, response.getChoices().get(0).getMessage());
+        return response;
+    }
+
+    private void addMessage(List<Message> messages, Message message) {
+        if(messages.size() > 10){
+            messages.remove(1);
+        }
+        messages.add(message);
     }
 }
