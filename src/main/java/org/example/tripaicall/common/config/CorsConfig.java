935eb1d8fb9d7fb6b8b802b2fbfea4aa3b3bfcfc
seate <86824703+seate@users.noreply.github.com>
2024-06-17 23:20:32 +0900

    fix: allowedOrigins를 allowedOriginPatterns로 수정

--- src/main/java/org/example/tripaicall/common/config/CorsConfig.java
+++ src/main/java/org/example/tripaicall/common/config/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정을 적용
-                .allowedOrigins("*") // 허용할 출처
+                .allowedOriginPatterns("*") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
-                .allowCredentials(false); // 자격 증명(쿠키 등) 허용 여부
+                .allowCredentials(true); // 자격 증명(쿠키 등) 허용 여부
     }
 }
