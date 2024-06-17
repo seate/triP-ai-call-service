6d635e0e7a55e6b37fadf8140dfd008a17f5a96a
seate <86824703+seate@users.noreply.github.com>
2024-06-18 02:09:14 +0900

    fix: 무조건 print하도록 테스트

--- src/main/java/org/example/tripaicall/common/config/CorsFilter.java
+++ src/main/java/org/example/tripaicall/common/config/CorsFilter.java
@@ -20,10 +20,19 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.setHeader("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
+        response.setHeader("Access-Control-Allow-Origin", "https://asdf-6e088.web.app/"); // 모든 도메인 허용
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // 모든 메서드 허용
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Max-Age", "3600"); // 프리플라이트 요청의 캐시 시간 설정
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+
+        System.out.println("CORS 필터 작동");
+        System.out.println("Access-Control-Allow-Origin" + response.getHeader("Access-Control-Allow-Origin"));
+        System.out.println("Access-Control-Allow-Methods" + response.getHeader("Access-Control-Allow-Methods"));
+        System.out.println("Access-Control-Allow-Headers" + response.getHeader("Access-Control-Allow-Headers"));
+        System.out.println("Access-Control-Allow-Credentials" + response.getHeader("Access-Control-Allow-Origin"));
+        System.out.println("Access-Control-Max-Age" + response.getHeader("Access-Control-Max-Age"));
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
