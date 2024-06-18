92e580c71e5d50a78ca910dceb3d140bb6e7cee3
haebyun <bhk4392@gmail.com>
2024-06-18 19:01:35 +0900

    refactor : JWT의 sub 클레임을 사용하지 않고 회원 DB에 저장 된 userID를 Param을 받도록 변경

--- src/main/java/org/example/tripaicall/chatbot/controller/ChatBotController.java
+++ src/main/java/org/example/tripaicall/chatbot/controller/ChatBotController.java
@@ -7,7 +7,6 @@ import org.example.tripaicall.openai.dto.Message;
 import org.example.tripaicall.openai.dto.response.ChatGPTResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +18,13 @@ public class ChatBotController {
     private final ChatBotService chatBotService;
 
     @PostMapping("/chat")
-    public String chatMessages(@RequestHeader("x-amzn-oidc-identity") String userId, @RequestParam String text) {
+    public String chatMessages(@RequestParam String userId, @RequestParam String text) {
         ChatGPTResponse response = chatBotService.chatWithAi(userId, text);
         return response.getChoices().get(0).getMessage().getContent();
     }
 
     @GetMapping("/chat")
-    public List<Message> getAllChatMessages(@RequestHeader("x-amzn-oidc-identity") String userId) {
+    public List<Message> getAllChatMessages(@RequestParam String userId) {
         return chatBotService.getMessages(userId);
     }
 }
